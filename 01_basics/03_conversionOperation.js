3d0705e433e7503f72318e0f63d6c1e50d98a22d
PatilAnuraag <Anuraagpatil123@gmail.com>
2023-10-22 17:58:36 +0000

    conversions and comparisions

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,60 @@
+let score = false;
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueNumber = Number(score);
+
+// console.log(typeof valueNumber);
+// console.log(valueNumber);
+
+// "33" => "33"
+// "33av" => NaN of type number
+// true => 1; false => 0;
+
+let isLoggedin = "string";
+
+let booleanIsLoggedIn = Boolean(isLoggedin);
+
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "string" => true
+
+let someNumber = 33;
+let stringNumber = String(someNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// *********************** Operations *************************
+
+let value = 3;
+let negValue = -value;
+// console.log(negValue);
+
+// console.log([2 + 2, 2 - 2, 2 * 2, 2 ** 3, 2 / 3, 2 % 3]);
+
+let str1 = "hello";
+let str2 = "tart";
+
+let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(((3 + 4) * 5) % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3;
+
+num1 = num2 = num3 = 2 + 2;
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
